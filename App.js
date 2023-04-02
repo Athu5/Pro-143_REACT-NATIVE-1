a37747114828c7424b032aeb0589d3d022ee2a54
Athu5 <84505685+Athu5@users.noreply.github.com>
2023-04-02 21:05:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeScreen from "./screens/Home";
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
